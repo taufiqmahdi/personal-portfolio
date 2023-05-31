bc70ecaf6c97b1a4bb5824746980fd9da435dd15
taufiqmahdi <57888672+taufiqmahdi@users.noreply.github.com>
2023-05-31 17:36:27 +0700

    update title metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Taufiq Mahdi's Portfolio</title>
+        <title>Taufiq Mahdi Portfolio</title>
         <meta name="description" content="Muhammad Taufiq Mahdi's Portfolio Page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
