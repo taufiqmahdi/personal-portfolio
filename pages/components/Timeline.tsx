5d6465d54e45e9c06553e3e402ea1458426e18d1
taufiqmahdi <57888672+taufiqmahdi@users.noreply.github.com>
2023-05-31 17:27:28 +0700

    Update Timeline.tsx

--- pages/components/Timeline.tsx
+++ pages/components/Timeline.tsx
@@ -8,15 +8,19 @@ const Timeline = (props: Props) => {
       <h3 className="text-4xl font-bold">Timeline</h3>
       <div className="flex flex-col gap-2 ">
         <div className="flex gap-4 items-center">
-          <h4 className="p-1 px-2 bg-black text-white rounded-lg font-bold">2022</h4>
-          <h4 className="text-lg font-semibold capitalize">Web Developer</h4>
+          <h4 className="p-1 px-2 bg-black text-white rounded-lg font-bold">
+            2022
+          </h4>
+          <h4 className="text-lg font-semibold capitalize">
+            Web Developer - PT. Pentacode Digital
+          </h4>
           <h5 className="text-gray-700">present</h5>
         </div>
         <p className="">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quasi,
-          repellat, aspernatur numquam earum tenetur sunt eligendi dolor placeat
-          natus sint quas! Molestiae omnis delectus optio ullam odit tempore
-          mollitia?
+          Developed websites for clients from various industries and purposes.
+          Also participates in almost every cycle of the development process,
+          such as brainstorming ideas, choosing the best approaches and write
+          codes.
         </p>
       </div>
     </div>
